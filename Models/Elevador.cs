6ef3dd09a6a56b294221750e4b274c5122f46088
Alencar Annater <alencarannater@gmail.com>
2021-12-29 12:38:00 -0300

    Primeiro commit

---
+++ Models/Elevador.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Models
+{
+    internal class Elevador
+    {
+        int qtdeAndaresTotal;
+        int qtdeCapacidadeTotal;
+        int qtdePessoasAtual;
+        int andarAtual;
+
+        //Recebe as informações iniciais da capacidade do elevador e quantidades de andares
+        public void Inicializar(int andares, int capacidade)
+        {
+            this.qtdeAndaresTotal = andares;
+            this.qtdeCapacidadeTotal = capacidade;
+        }
+
+        //Controla a entrada de pessoas no elevador
+        //Emite aviso se a entrada foi permitida ou já atingiu a capacidade permitida.
+        public void Entrar()
+        {
+            if (qtdePessoasAtual < qtdeCapacidadeTotal)
+            {
+                qtdePessoasAtual++;
+                Console.WriteLine("Entrada permitida! Capacidade maxima de pessoas é " + qtdeCapacidadeTotal + " vagas disponiveis: " + (qtdeCapacidadeTotal - qtdePessoasAtual));
+            }
+            else Console.WriteLine("Entrada negada! O máximo permitido é " + qtdeCapacidadeTotal);
+        }
+        //Controla a saída de pessoas do elevador
+        //Emite aviso se a saída foi permitida ou o elevador esta vazio. 
+        public void Sair()
+        {
+
+            if (qtdePessoasAtual > 0)
+            {
+                qtdePessoasAtual--;
+                string msgSaida = qtdePessoasAtual == 0 ? "Saída permitida! Elevador esta vazio!" : "Saída permitida! Quantidade de pessoas no elevador: " + qtdePessoasAtual;
+                Console.WriteLine(msgSaida);
+            }
+            else
+                Console.WriteLine("O elevador está vazio!");
+        }
+        //Permite subir um andar
+        //Avisa o andar atual ou se já esta no último andar.
+        public void Subir()
+        {
+            if (andarAtual < qtdeAndaresTotal)
+            {
+                andarAtual++;
+                Console.WriteLine("O prédio possui " + qtdeAndaresTotal + " andares. O elevador subiu para o andar: " + andarAtual);
+            }
+            else Console.WriteLine("Você está no último andar: " + andarAtual);
+
+        }
+        //Permite descer um andar
+        //Avisa o andar atual ou se já esta no térreo.
+        public void Descer()
+        {
+            if (andarAtual != 0)
+            {
+                andarAtual--;
+                string msgDescer = andarAtual == 0 ? "O elevador está no térreo" : "O prédio possui " + qtdeAndaresTotal + " andares. O elevador desceu para o andar: " + andarAtual;
+                Console.WriteLine(msgDescer);
+            }
+            else Console.WriteLine("O elevador está no térreo! ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using ProjetoElevador.Models;
+using System;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicita a informação de quantos andares existem.
+            Console.WriteLine("Informe quantos andares existem no prédio?");
+            int qtdeAndares = int.Parse(Console.ReadLine());
+            //Solcita a quantidade de pessoas que o elevador suporta.
+            Console.WriteLine("Informe quantas pessoas o elevador suporta?");
+            int qtdePessoasCapacidade = int.Parse(Console.ReadLine());
+
+            Elevador elevador = new Elevador();
+            //Passa a informações de andares e quantidade de pessoas para a classe Elevador
+            elevador.Inicializar(qtdeAndares, qtdePessoasCapacidade);
+            Console.Clear();
+
+            bool opcao = true;
+            do
+            {
+                //Opções de escolha para o usuário
+                Console.WriteLine(@"Escolha uma opção:
+                                1 - Entrar
+                                2 - Sair
+                                3 - Subir
+                                4 - Descer
+                                5 - Encerrar o sistema
+                                ");
+                string opcaoEscolhida = Console.ReadLine();
+                Console.Clear();
+
+                switch (opcaoEscolhida)
+                {
+                    case "1": elevador.Entrar(); break;
+                    case "2": elevador.Sair(); break;
+                    case "3": elevador.Subir(); break;
+                    case "4": elevador.Descer(); break;
+                    case "5": opcao = false; break;
+                    default: Console.WriteLine("Escolha incorreta"); break;
+                }
+            } while (opcao);
+        }
+    }
+}
